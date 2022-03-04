d0d5bfa09e904ddc4c4291508aad5bb30a51548b
Hector Ventura <hventura@salmonserver.com>
2022-03-04 10:18:20 -0500

    V1

---
+++ src/main/java/com/example/satnotificaciones/SatNotificacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.satnotificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatNotificacionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatNotificacionesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/satnotificaciones/SatNotificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.satnotificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatNotificacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
